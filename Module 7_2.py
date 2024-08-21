1cd1c2cdc691e6ad403609a064d5d01fafca649c
skynavi <skynavi@mail.ru>
2024-08-21 22:34:13 +0300

    2108 выстрадал)

--- Module 7_2.py
+++ Module 7_2.py
@@ -2,20 +2,14 @@ from pprint import pprint
 
 
 def custom_write(file_name, strings):
-    #strings_positions = {}
+    strings_positions = {}
     for i in range(len(strings)):
         file = open(file_name, 'a', encoding='utf-8')
-        #print(i+1)
-        strings_positions = {(i+1, file.tell()): strings[i]}
-        print(strings_positions)
+        strings_positions.update({(i+1, file.tell()): strings[i]})
         file.write(strings[i] + '\n')
         file.close()
+    return strings_positions
 
-        #file = open(file_name, 'r', encoding='utf-8')
-        #byte_ = (file.tell())
-        #pprint(file.read())
-        #byte_ = (file.tell())
-        #file.close()
 
 
 info = [
